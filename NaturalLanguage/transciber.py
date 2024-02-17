00af0f172128486a62203d1acc844e931e7c0bba
AshwinSant5 <Ashwinsant5@gmail.com>
2024-02-16 21:47:28 -0500

    removed api key

--- NaturalLanguage/transciber.py
+++ NaturalLanguage/transciber.py
@@ -3,7 +3,7 @@ from openai import OpenAI
 #api_key = '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'
 #model_id = 'whisper-1'
 
-client = OpenAI(api_key = '') #REPLACE API KEY WITH NEW ONE FROM OPENAI API PAGE
+client = OpenAI(api_key = '') #REPLACE API KEY WITH NEW ONE FROM OPENAI API WEBPAGE
 
 media_file_path = 'test3.mp3'
 media_file = open(media_file_path, 'rb')
